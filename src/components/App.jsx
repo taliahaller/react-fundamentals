8c4da1c5fdb782630786b7fa58dbf97a51470452
Talia Grace <taliahaller@Talias-iMac.local>
2018-10-16 19:03:02 -0700

    finish react fundamentals set up through 'components'

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+
+import React from "react";
+import TicketList from "./TicketList";
+import Header from "./Header";
+
+function App(){
+  return (
+    <div>
+      <Header/>
+      <TicketList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Ticket.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Ticket() {
+  return (
+    <div>
+      <h3>3a</h3>
+      <h3>Thato and Haley</h3>
+      <p><em>Firebase entries not saving!</em></p>
+      <hr/>
+    </div>
+  );
+}
+
+export default Ticket;
